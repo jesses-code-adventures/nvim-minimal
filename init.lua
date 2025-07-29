1817aec92fa1e58482cff6c477ecaf0d669675b6
jesse <jesse@rapidpaylegal.com>
2025-07-29 16:58:47 +1000

    format, add templ and tailwindcss lsp

--- init.lua
+++ init.lua
@@ -22,34 +22,34 @@ o.updatetime = 10
 o.colorcolumn = "0"
 o.cmdheight = 0
 vim.filetype.add {
-    extension = {
-        templ = "templ",
-    },
+	extension = {
+		templ = "templ",
+	},
 }
 o.termguicolors = true
 
 -- get plugins
-vim.pack.add{
-  { src = "https://github.com/neovim/nvim-lspconfig" },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-  { src = "https://github.com/NLKNguyen/papercolor-theme" },
-  -- { src = "https://github.com/kvrohit/rasmus.nvim" },
-  -- { src = "https://github.com/kepano/flexoki-neovim" },
-  { src = "https://github.com/stevearc/oil.nvim" },
-  { src = "https://github.com/ibhagwan/fzf-lua" },
-  { src = "https://github.com/lewis6991/gitsigns.nvim" },
-  { src = "https://github.com/tpope/vim-fugitive" },
-  { src = "https://github.com/supermaven-inc/supermaven-nvim" },
-  { src = "https://github.com/vrischmann/tree-sitter-templ" },
-  { src = "https://github.com/diepm/vim-rest-console" },
-  { src = "https://github.com/jesses-code-adventures/dotenv.nvim" },
-  { src = "https://github.com/timwmillard/uuid.nvim" },
+vim.pack.add {
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/NLKNguyen/papercolor-theme" },
+	-- { src = "https://github.com/kvrohit/rasmus.nvim" },
+	-- { src = "https://github.com/kepano/flexoki-neovim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/ibhagwan/fzf-lua" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+	{ src = "https://github.com/tpope/vim-fugitive" },
+	{ src = "https://github.com/supermaven-inc/supermaven-nvim" },
+	{ src = "https://github.com/vrischmann/tree-sitter-templ" },
+	{ src = "https://github.com/diepm/vim-rest-console" },
+	{ src = "https://github.com/jesses-code-adventures/dotenv.nvim" },
+	{ src = "https://github.com/timwmillard/uuid.nvim" },
 }
 
 -- lsp & diagnostics
 require("diagnostics")
 require("lsp")
-vim.lsp.enable({ "lua_ls", "ruff", "gopls", "pyright" })
+vim.lsp.enable({ "lua_ls", "ruff", "gopls", "pyright", "templ", "html", "tailwindcss" })
 
 vim.cmd("colorscheme PaperColor")
 vim.cmd("hi statusline guibg=NONE")
@@ -58,23 +58,23 @@ vim.cmd("hi StatusLineNC guibg=NONE")
 local lspconfig = require("lspconfig")
 
 lspconfig.lua_ls.setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "bit", "vim", "it", "describe", "before_each", "after_each", "os", "require" },
-      },
-      library = {
-        vim.fn.expand("$VIMRUNTIME/lua"),
-      }
-    }
-  },
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "bit", "vim", "it", "describe", "before_each", "after_each", "os", "require" },
+			},
+			library = {
+				vim.fn.expand("$VIMRUNTIME/lua"),
+			}
+		}
+	},
 })
 
 -- setup plugins
 require("dotenv").setup({
 	overrides = { ".env", ".local.env", ".env.local", ".local.mine.env", ".env.mine" },
 })
-require("oil").setup( { view_options = { show_hidden = true } })
+require("oil").setup({ view_options = { show_hidden = true } })
 require("gitsigns").setup({
 	signs = {
 		add = { text = "+" },
@@ -106,41 +106,49 @@ g.vrc_set_default_mappings = 0
 g.vrc_response_default_content_type = "application/json"
 g.vrc_output_buffer_name = "_OUTPUT.json"
 g.vrc_auto_format_response_patterns = { json = "jq" }
+g.vrc_show_command = 1
 
 -- commands
 vim.api.nvim_create_user_command('Todos', function()
-    require('fzf-lua').grep { search = [[TODO:|todo!\(.*\)|HACK:|hack!\(.*\)]], no_esc = true }
+	require('fzf-lua').grep { search = [[TODO:|todo!\(.*\)|HACK:|hack!\(.*\)]], no_esc = true }
 end, { desc = 'Grep TODOs', nargs = 0 })
 
 vim.api.nvim_create_user_command('Scratch', function()
-    vim.cmd('bel 10new')
-    local buf = vim.api.nvim_get_current_buf()
-    for name, value in pairs {
-        filetype = 'scratch',
-        buftype = 'nofile',
-        bufhidden = 'wipe',
-        swapfile = false,
-        modifiable = true,
-    } do
-        vim.api.nvim_set_option_value(name, value, { buf = buf })
-    end
+	vim.cmd('bel 10new')
+	local buf = vim.api.nvim_get_current_buf()
+	for name, value in pairs {
+		filetype = 'scratch',
+		buftype = 'nofile',
+		bufhidden = 'wipe',
+		swapfile = false,
+		modifiable = true,
+	} do
+		vim.api.nvim_set_option_value(name, value, { buf = buf })
+	end
 end, { desc = 'Open a scratch buffer', nargs = 0 })
 
 vim.cmd([[
   au BufNewFile,BufRead *.env.* set filetype=sh
 ]])
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'templ',
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
+
 -- when yanking, highlight the yanked text
 local yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
 vim.api.nvim_create_autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+	group = yank_group,
+	pattern = '*',
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = 'IncSearch',
+			timeout = 40,
+		})
+	end,
 })
 
 -- keybinds
@@ -152,25 +160,36 @@ vim.keymap.set("n", "<leader>yy", [["+Y]], { desc = "Yank line to system clipboa
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
 -- vim.keymap.set("n", "<leader>gt", [[:vsplit<CR><C-w>L:vertical resize -60<CR>:terminal<CR>]], { desc = "Open terminal in split pane" })
-vim.keymap.set("n", "<leader>gt", [[:split<CR><C-w>j:resize 10<CR>:terminal<CR>]], { desc = "Open terminal in split pane" })
+vim.keymap.set("n", "<leader>gt", [[:split<CR><C-w>j:resize 10<CR>:terminal<CR>]],
+	{ desc = "Open terminal in split pane" })
 vim.keymap.set('t', '<Esc><Esc>', [[<C-\><C-n>]], { noremap = true, silent = true, desc = "Exit terminal mode" })
 vim.keymap.set('n', '<leader>td', '<cmd>Todos<cr>', { desc = "Search TODOs" })
 vim.keymap.set("n", "<leader>cf", "<cmd>:let @+ = expand('%')<CR>", { desc = "Copy current file path" })
 
+-- keybinds (lsp)
+vim.keymap.set("n", "<leader>F", function() vim.lsp.buf.format { async = true } end, { desc = "Format buffer" })
+
 -- keybinds (fzf-lua)
-vim.keymap.set("n", "<leader>ds", function() require("fzf-lua").lsp_document_symbols() end, { desc = "[FZF] LSP Document symbols" })
-vim.keymap.set("n", "<leader>xx", function() require("fzf-lua").diagnostics_workspace() end, { desc = "[FZF] Workspace diagnostics" })
+vim.keymap.set("n", "<leader>ds", function() require("fzf-lua").lsp_document_symbols() end,
+	{ desc = "[FZF] LSP Document symbols" })
+vim.keymap.set("n", "<leader>xx", function() require("fzf-lua").diagnostics_workspace() end,
+	{ desc = "[FZF] Workspace diagnostics" })
 vim.keymap.set("n", "<leader>ps", function() require("fzf-lua").grep() end, { desc = "[FZF] Grep" })
 vim.keymap.set("n", "<leader>vh", function() require("fzf-lua").help_tags() end, { desc = "[FZF] Search help" })
 vim.keymap.set("n", "<leader>gf", function() require("fzf-lua").git_files() end, { desc = "[FZF] Fuzzy find git files" })
 vim.keymap.set("n", "<leader>km", function() require("fzf-lua").keymaps() end, { desc = "[FZF] Fuzzy find keymaps" })
 vim.keymap.set("n", "<leader>pb", function() require("fzf-lua").buffers() end, { desc = "[FZF] Fuzzy find buffers" })
-vim.keymap.set("n", "<leader>pf", function() require("fzf-lua").files({
-	cmd = "fd -t f -E '.git' -E '**/*.sql.go' -E '**/*_templ.go' -E '**/*mocks.go' -E '**/*mocks_test.go' -E '_tmp'"
-}) end, { desc = "Fuzzy find files" })
-vim.keymap.set("n", "<leader>lg", function() require("fzf-lua").live_grep({
-	cmd = "rg -. -g '!*_mocks.go' -g '!*mocks_test.go' -g '!.git' -g '!**/*.sql.go' -g '!*_templ.go' -g '!_tmp' --column -n"
-}) end, { desc = "Grep (live)" })
+vim.keymap.set("n", "<leader>pf", function()
+	require("fzf-lua").files({
+		cmd = "fd -t f -E '.git' -E '**/*.sql.go' -E '**/*_templ.go' -E '**/*mocks.go' -E '**/*mocks_test.go' -E '_tmp'"
+	})
+end, { desc = "Fuzzy find files" })
+vim.keymap.set("n", "<leader>lg", function()
+	require("fzf-lua").live_grep({
+		cmd =
+		"rg -. -g '!*_mocks.go' -g '!*mocks_test.go' -g '!.git' -g '!**/*.sql.go' -g '!*_templ.go' -g '!_tmp' --column -n"
+	})
+end, { desc = "Grep (live)" })
 
 -- keybinds (vim-rest-console)
 vim.keymap.set("n", "<leader>r", ":call VrcQuery()<CR>", { desc = "Make request - vim rest console" })
