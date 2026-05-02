8df6cc952813784ae8eb0c84ae79ecd31ab6e5f6
Jesse Williams <jesse@negativespacesounds.com>
2026-05-02 20:12:47 +1000

    rm head

--- lua/custom_commands.lua
+++ lua/custom_commands.lua
@@ -18,11 +18,11 @@ vim.api.nvim_create_user_command('Scratch', function()
 end, { desc = 'Open a scratch buffer', nargs = 0 })
 
 vim.api.nvim_create_user_command('Dom', function()
-	vim.cmd('DiffviewOpen origin/main...HEAD')
+	vim.cmd('DiffviewOpen origin/main')
 end, { desc = 'Diffview against origin/main', nargs = 0 })
 
 vim.api.nvim_create_user_command('Dm', function()
-	vim.cmd('DiffviewOpen main...HEAD')
+	vim.cmd('DiffviewOpen main')
 end, { desc = 'Diffview against main', nargs = 0 })
 
 vim.api.nvim_create_user_command('Dc', function()
