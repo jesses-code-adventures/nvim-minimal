a186dfc3ea7926c9ffd7234c65327e1f3a684893
Jesse Williams <jesse@negativespacesounds.com>
2026-05-10 21:45:18 +1000

    move dv cmds to dv

--- lua/dv.lua
+++ lua/dv.lua
@@ -34,3 +34,59 @@ require("diffview").setup({
 		},
 	},
 })
+
+vim.api.nvim_create_user_command('Dom', function()
+	vim.cmd('DiffviewOpen --untracked-files=all origin/main')
+end, { desc = 'Diffview against origin/main', nargs = 0 })
+
+vim.api.nvim_create_user_command('Dm', function()
+	vim.cmd('DiffviewOpen --untracked-files=all main')
+end, { desc = 'Diffview against main', nargs = 0 })
+
+vim.api.nvim_create_user_command('Dc', function()
+	vim.cmd('DiffviewClose')
+end, { desc = 'Close Diffview', nargs = 0 })
+
+vim.api.nvim_create_user_command('Do', function()
+	vim.cmd('DiffviewOpen')
+end, { desc = 'Open Diffview against index', nargs = 0 })
+
+local function github_diff_highlights()
+	local highlights = {
+		DiffAdd = { bg = "#1f3d2a", fg = "#b7dfb9" },
+		DiffChange = { bg = "#3a2f1f" },
+		DiffDelete = { bg = "#4a2528", fg = "#e8b9b7" },
+		DiffText = { bg = "#5a4724", fg = "#eadca6" },
+		Folded = { bg = "#161b22", fg = "#8b949e" },
+		FoldColumn = { bg = "#0d1117", fg = "#8b949e" },
+		TabLineSel = { bg = "#bc5215", fg = "#100f0f" },
+		diffAdded = { fg = "#87a987" },
+		diffRemoved = { fg = "#bf8f8f" },
+		diffChanged = { fg = "#d29922" },
+		DiffviewNormal = { fg = "#c9d1d9" },
+		DiffviewCursorLine = { bg = "#2a2f36" },
+		DiffviewFilePanelTitle = { fg = "#8b949e", bold = true },
+		DiffviewFilePanelFileName = { fg = "#c9d1d9" },
+		DiffviewFilePanelSelected = { fg = "#ffffff", bold = true },
+		DiffviewFilePanelPath = { fg = "#8b949e" },
+		DiffviewFilePanelInsertions = { fg = "#87a987" },
+		DiffviewFilePanelDeletions = { fg = "#bf8f8f" },
+		DiffviewDiffAdd = { bg = "#1f3d2a", fg = "#b7dfb9" },
+		DiffviewDiffChange = { bg = "#3a2f1f" },
+		DiffviewDiffChangeAdd = { bg = "#1f3d2a", fg = "#b7dfb9" },
+		DiffviewDiffChangeDelete = { bg = "#4a2528", fg = "#e8b9b7" },
+		DiffviewDiffText = { bg = "#5a4724", fg = "#eadca6" },
+		DiffviewDiffTextAdd = { bg = "#2d5a3a", fg = "#e6f4e8" },
+		DiffviewDiffTextDelete = { bg = "#6b3036", fg = "#f4e1e1" },
+		DiffviewDiffAddAsDelete = { bg = "#4a2528", fg = "#e8b9b7" },
+		DiffviewDiffDelete = { bg = "#4a2528", fg = "#e8b9b7" },
+		DiffviewDiffDeleteDim = { bg = "NONE", fg = "NONE" },
+	}
+
+	for group, attrs in pairs(highlights) do
+		vim.api.nvim_set_hl(0, group, attrs)
+	end
+end
+
+github_diff_highlights()
+vim.api.nvim_create_autocmd("ColorScheme", { callback = github_diff_highlights })
