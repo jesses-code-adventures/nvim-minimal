82224b2e3f72c6b76b2e4d7cdca822b823ce8167
Jesse Williams <jesse@negativespacesounds.com>
2026-05-11 22:15:21 +1000

    fix expansion

--- lua/plugin_dev.lua
+++ lua/plugin_dev.lua
@@ -3,13 +3,14 @@
 ---@param url string
 ---@param version string?
 local function handle_dev_plugin(dev, dir, url, version)
-	if dev and vim.fn.isdirectory(dir) == 1 then
-		vim.opt.runtimepath:prepend(vim.fn.expand(dir))
+	local expanded_dir = vim.fn.expand(dir)
+	if dev and vim.fn.isdirectory(expanded_dir) == 1 then
+		vim.opt.runtimepath:prepend(expanded_dir)
 	else
 		vim.pack.add({ { src = url, version = version } })
 	end
 end
 
 handle_dev_plugin(false, "~/coding/contrib/diffview.nvim", "https://github.com/dlyongemallo/diffview.nvim", "main")
-handle_dev_plugin(true, "~/coding/personal/diffview-pr.nvim", "https://github.com/dlyongemallo/diffview.nvim")
+handle_dev_plugin(true, "~/coding/personal/diffview-pr.nvim", "https://github.com/jesses-code-adventures/diffview-pr.nvim")
 handle_dev_plugin(false, "~/coding/personal/pipeline.nvim", "https://github.com/jesses-code-adventures/pipeline.nvim")
